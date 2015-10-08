2ba6587e919637da8538e49c819d24f6bac9dd61
msAngeles <arkangel_nissi@hotmail.com>
2015-10-08 16:35:35 -0500

    Create suma.java

---
+++ suma.java
@@ -0,0 +1,10 @@
+public class Suma{
+
+  public static void main(String args[]){
+    Scanner entrada=new Scanner(System.in);
+    int numero=entrada.nextInt();
+    int numero2=entrada.nextInt();
+    
+    System.out.println("La suma de los numeros es: "+ (numero+numero2));
+  }
+}
